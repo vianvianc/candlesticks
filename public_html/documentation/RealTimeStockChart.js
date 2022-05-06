c63accdc19cb5779b7d8c963a285b7cb167087d8
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-05-05 21:54:46 -0600

    added chart style

--- public_html/documentation/RealTimeStockChart.js
+++ public_html/documentation/RealTimeStockChart.js
@@ -6,8 +6,23 @@ define(["require", "exports", 'Scripts/MindFusion.Charting'], function (require,
    let Drawing = m.MindFusion.Charting.Drawing;
 
    let stockChart = new Controls.CandlestickChart(document.getElementById('stockChart'));
-   stockChart.title = "The Big Corporation";
+   stockChart.title = "IBM";
    stockChart.theme.titleFontSize = 16;
+   stockChart.candlestickWidth = 12;
+   stockChart.showLegend = false;
+   stockChart.showXCoordinates = false;
+   stockChart.xAxis.minValue = 0;
+   stockChart.xAxis.interval = 1;
+   stockChart.xAxis.maxValue = 40;
+   stockChart.xAxis.title = "Time";
+   stockChart.yAxis.title = "Price";
+
+   stockChart.gridType = Charting.GridType.Horizontal;
+   stockChart.theme.gridColor1 = new Drawing.Color("#ffffff");
+   stockChart.theme.gridColor2 = new Drawing.Color("#fafafa");
+   stockChart.theme.gridLineColor = new Drawing.Color("#cecece");
+   stockChart.theme.gridLineStyle = new Drawing.DashStyle.Dash;
+
 
    let dataList = new Collections.List();
    updateStock();
@@ -17,7 +32,7 @@ define(["require", "exports", 'Scripts/MindFusion.Charting'], function (require,
       $.getJSON("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=1min&apikey=6VD45ZWE08EC0J3H", function(json) {
          let times = json["Time Series (1min)"];
          let update = false;
-         if stockChart.series.count() > 0 )
+         if (stockChart.series.count() > 0 )
              update = true;
             for (let time in times){
                let stock_info = times[time];
