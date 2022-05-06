134ced4acffd9810ef87f73a74f607b6bc67ac4e
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-05-05 21:23:43 -0600

    added api key

--- public_html/documentation/RealTimeStockChart.js
+++ public_html/documentation/RealTimeStockChart.js
@@ -6,8 +6,34 @@ define(["require", "exports", 'Scripts/MindFusion.Charting'], function (require,
    let Drawing = m.MindFusion.Charting.Drawing;
 
    let stockChart = new Controls.CandlestickChart(document.getElementById('stockChart'));
-   chart.title = "The Big Corporation";
-   chart.theme.titleFontSize = 16;
+   stockChart.title = "The Big Corporation";
+   stockChart.theme.titleFontSize = 16;
+
+   let dataList = new Collections.List();
+   updateStock();
+
+   function updateStock()
+   {
+      $.getJSON("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=1min&apikey=6VD45ZWE08EC0J3H", function(json) {
+         let times = json["Time Series (1min)"];
+         let update = false;
+         if stockChart.series.count() > 0 )
+             update = true;
+            for (let time in times){
+               let stock_info = times[time];
+               let dataItem = new Charting.StockPrice(stock_info["1. open"], stock_info["4. close"], stock_info["3. low"], stock_info["2. high"], new Date(time));
+
+               dataList.add(dataItem);
+            }
+            let series = new Charting.StockPriceSeries(dataList);
+            series.dateTimeFormat = Charting.DateTimeFormat.ShortTime;
+
+            let data = new Collections.ObservableCollection();
+            data.add(series);
+            stockChart.series = data;
+            stockChart.draw();
+      })
+   }
 
 
 });
