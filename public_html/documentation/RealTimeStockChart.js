db4e87a31ce1ba8349cea32a2404146cdd2f4fb9
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-05-05 20:36:45 -0600

    configured

---
+++ public_html/documentation/RealTimeStockChart.js
@@ -0,0 +1,13 @@
+define(["require", "exports", 'Scripts/MindFusion.Charting'], function (require, exports, m) {
+   "use strict" ;
+   let Charting = m.MindFusion.Charting;
+   let Controls = m.MindFusion.Charting.Controls;
+   let Collections = m.MindFusion.Charting.Collections;
+   let Drawing = m.MindFusion.Charting.Drawing;
+
+   let stockChart = new Controls.CandlestickChart(document.getElementById('stockChart'));
+   chart.title = "The Big Corporation";
+   chart.theme.titleFontSize = 16;
+
+
+});
