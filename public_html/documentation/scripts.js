ef83a7ebbfc866dd6f013467c1d7e153c82e87f9
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-05-08 13:13:01 -0600

    added styles.css

---
+++ public_html/documentation/scripts.js
@@ -0,0 +1,20 @@
+const body = document.body
+const div = document.querySelector('div')
+const spanHi =document.querySelector('#hi')
+const spanBye =document.querySelector('#bye')
+
+console.log(spanHi.title)
+
+//div.removeChild(spanHi)
+
+//spanBye.remove( )
+//div.append(spanBye)
+
+//const div = document.createElement('div')
+//div.innerText = "hi"
+//div.textContent = "hi again"
+//const strong = document.createElement('strong')
+//strong.innerText = 'Hi Again'
+//div.append(strong)
+//div.innerHTML = "<strong>Hi AGAIN</strong> "
+//body.append(div)
