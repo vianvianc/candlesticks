b06bfa77e5e8978d66df9976e51f93cfbeef10f8
Vian Charbonneau <102205118+vianvianc@users.noreply.github.com>
2022-05-20 09:09:59 -0600

    restructured file directories added docker file frontend and backend

--- src/index.ts
+++ src/index.ts
@@ -19,8 +19,8 @@ const validation = [
 ]
 const app: Application = express()
 app.use(morgan('dev'))
-app.use(express.json)
-app.use(bodyParser.urlendocded({extended: false}))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
 const recaptcha = new Recaptcha(process.env.RECAPTCHA_SITE_KEY, process.env.RECAPTCHA_SECRET_KEY)
@@ -31,9 +31,51 @@ const handleGetRequest = (request: Request, response: Response) => {
     return response.json('This works')
 }
 
+const handlePostRequest = (request: Request, response: Response) => {
+
+    response.append('Content-Type', 'text/html')
+
+    //@ts-ignore
+    if (request.recaptcha.error) {
+        return response.send(
+            `<div class='alert alert-danger' role='alert'><strong>Oh No</strong> There was a recaptcha error.</div>`
+        )
+    }
+
+    const errors = validationResult(request)
+
+    if (errors.isEmpty() === false) {
+        const currentError = errors.array()[0]
+        return response.send(
+            `<div class='alert alert-danger' role='alert'><strong>uh-oh</strong> ${currentError.msg}</div>`
+        )
+    }
+    const {name, email, subject, message} = request.body
+   // console.log(request.body.name)  //object destructuring
+    const mailgunData = {
+        to: process.env.MAIL_RECIPIENT,
+        from: `${name} <postmaster@${process.env.MAILGUN_DOMAIN}>`
+        subject: `${email}: ${subject}`,
+        text: message
+    }
+    mailgunClient.messages.create(process.env.MAILGUN_DOMAIN, mailgunData)
+        .then((msg: any) =>
+        response.send(
+            `<div class='alert alert -success' role='alert'> Email Successfully Sent</div>`
+        )
+        )
+        .catch((error: any) =>
+         response.send(
+        `<div class='alert alert-danger' role='alert'><strong>nooooo</strong> Email Failed. Please try again.</div>`
+        ))
+}
+
 const indexRoute = express.Router()
 indexRoute.route('/')
     .get(handleGetRequest)
+    .post(recaptcha.middleware.verify, validation, handlePostRequest)
+
+
 app.use('/apis', indexRoute)
 
 
